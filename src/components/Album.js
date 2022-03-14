13e98f574297b6d07d9b73ffec1176f04eef562e
jfilinho <filinhosouza10@gmail.com>
2022-03-14 10:30:26 -0300

    album modificado

--- src/components/Album.js
+++ src/components/Album.js
@@ -18,34 +18,9 @@ function Album(){
                   />
                   <title>Placeholder</title>
                   <rect width="100%" height="100%" fill="#55595c"></rect>
-                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                    Thumbnail
-                  </text>
+                 
 
-                  <div className="card-body">
-                    <p className="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          View
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          Edit
-                        </button>
-                      </div>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </div>
+                  
                 </div>
               </div>
               <div className="col">
