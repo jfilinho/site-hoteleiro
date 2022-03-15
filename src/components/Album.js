c04a5d3ebd160de5fcd3c9b3d6ae8224e30c2fae
jfilinho <filinhosouza10@gmail.com>
2022-03-15 17:16:59 -0300

    rodape

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,156 +1,156 @@
-function Album(){
-    return (
-      <div>
-        <h2 className="titulos">ACOMODAÇÕES</h2>
-        <div className="album py-sm-0 bg-light">
-          <div className="container-fluid">
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto1.jpg"
-                    alt="quarto1"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+function Album() {
+  return (
+    <div>
+      <h2 className="titulos">ACOMODAÇÕES</h2>
+      <div className="album py-sm-0 bg-light">
+        <div className="container-fluid">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto1.jpg"
+                  alt="quarto1"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto2.jpg"
-                    alt="quarto 2"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto2.jpg"
+                  alt="quarto 2"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto3.jpg"
-                    alt="quarto 3"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto3.jpg"
+                  alt="quarto 3"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
+            </div>
 
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto4.jpg"
-                    alt="quarto 4"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto4.jpg"
+                  alt="quarto 4"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto5.jpg"
-                    alt="quarto 5"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto5.jpg"
+                  alt="quarto 5"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto6.jpg"
-                    alt="quaro 6"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto6.jpg"
+                  alt="quaro 6"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
+            </div>
 
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto7.jpg"
-                    alt="quarto 7"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto7.jpg"
+                  alt="quarto 7"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto8.jpg"
-                    alt="quarto 8"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto8.jpg"
+                  alt="quarto 8"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
-              <div className="col">
-                <div className="card shadow-sm photo-album">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    src="./img/quarto9.jpg"
-                    alt="quarto 9"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  />
-                </div>
+            </div>
+            <div className="col">
+              <div className="card shadow-sm photo-album">
+                <img
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  src="./img/quarto9.jpg"
+                  alt="quarto 9"
+                  aria-label="Placeholder: Thumbnail"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                />
               </div>
-              <div className="whatsapp-link">
-                <a
-                  href="https://wa.me/5511974751884?text=Olá,%20gostaria%20de%20mais%20
+            </div>
+            <div className="whatsapp-link">
+              <a
+                href="https://wa.me/5511974751884?text=Olá,%20gostaria%20de%20mais%20
               informações%20sobre%20hospedagem."
-                  target="blank"
-                  className="icons-contact"
-                >
-                  <img
-                    src="./img/whatsapp.png"
-                    alt="icone WhatsApp"
-                    className="whatsapp"
-                  />
-                </a>
-              </div>
+                target="blank"
+                className="icons-contact"
+              >
+                <img
+                  src="./img/whatsapp.png"
+                  alt="icone WhatsApp"
+                  className="whatsapp"
+                />
+              </a>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 export default Album;
