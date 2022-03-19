0419f9dab1aa32e46f208903358654443e78b46a
jfilinho <filinhosouza10@gmail.com>
2022-03-19 19:25:34 -0300

    icones redes sociais

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -1,20 +1,45 @@
 import { FaHeart } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
+
+
 
 function Rodape() {
   return (
     <footer className="footer">
       <div className="footer__social">
-        <a href="https://www.instagram.com/speak.up.tech/">
+        <a
+          href="https://www.linkedin.com/in/jo%C3%A3o-filho-souza-silva-734773165/"
+          className=" link-icone"
+          title="linkedin"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/jfilinho"
+          className=" link-icone"
+          title="github"
+        >
+          <AiFillGithub size="40" color="#fff" />
+        </a>
+
+        <a href="https://www.instagram.com/filho_souzaa/">
           <FaInstagram size="40" color="#fff" />
         </a>
-        <a href="https://www.youtube.com/c/SpeakupTech">
-          <FaYoutube size="40" color="#fff" />
+
+        <a
+          href="https://www.facebook.com/jfilhossouza"
+          className=" link-icone"
+          title="Facebook"
+        >
+          <FaFacebook />
         </a>
       </div>
       <p className="footer__copyright">
-        Made with by <FaHeart color="#B51942" /> Speak up Tech
+        Desenvolvido por <FaHeart color="#B51942" /> João Filho
       </p>
     </footer>
   );
